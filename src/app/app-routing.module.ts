69573dfd98e77699fd635206ac9b63effa26e848
yacin <yacine.toufiki@gmail.coml>
2022-08-15 01:27:43 +0200

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {TestComponent} from './test/test.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'test', component: TestComponent },
+
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent },
+
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+
+  //{path: 'cart-details', component: CartDetailsComponent},
+
+  //{path: 'checkout', component: CheckoutComponent},
+
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { TestComponent } from './test/test.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    TestComponent,
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
